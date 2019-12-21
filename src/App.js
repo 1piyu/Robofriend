02d51c8404e1fff996eb584959e3b905d94103f2
Pranali <pranali8506@gmail.com>
2019-12-21 17:48:06 +0530

    deploying SPA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardList from './Component/CardList/CardList';
 import './App.css';
+//import {CardList} from './Component/CardList/CardList.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state={
+      users:[],
+      searchfeild:''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then(response=>response.json())
+     .then(data=> this.setState({users:data}),console.log(this.state.users));
+  }
+
+
+
+  render(){
+
+console.log(this.state.users)
+
+
+
+const filteredRobot=this.state.users.filter(searchfeild => searchfeild.name.toLowerCase().includes(this.state.searchfeild));
+
+    return(
+        <div className="App">
+          <h1>Robofriends</h1>
+          <input type="text"  placeholder="search robots"  onChange={(e)=>this.setState({searchfeild:e.target.value})}/>
+    
+          <CardList users={filteredRobot}/>
+        </div>
+      );
+  }
 }
 
 export default App;
+
+
+
---
+++ src/Component/CardList/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./CardList.css';
+import Card from'../Card/card';
+
+const CardList=({users})=>{
+
+return(
+
+<div className="list">
+
+	{users.map(user => <Card key={user.id} id={user.id} name={user.name} email={user.email}/>)}
+	</div>
+
+	);
+
+}
+export default CardList;

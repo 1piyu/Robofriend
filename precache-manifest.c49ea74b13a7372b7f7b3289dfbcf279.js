b98b2f9df087dbbb8ce191a9af1f5e4957877b8b
Pranali <pranali8506@gmail.com>
2019-12-22 11:43:57 +0530

    Updates

---
+++ precache-manifest.c49ea74b13a7372b7f7b3289dfbcf279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d2567b3241e9a417baa9fcd5e113c0",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "c3212b2fe260120688c8",
+    "url": "/Robofriend/static/css/main.d1afb4a7.chunk.css"
+  },
+  {
+    "revision": "0beaab8d193afed4ebf2",
+    "url": "/Robofriend/static/js/2.3c805321.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriend/static/js/2.3c805321.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3212b2fe260120688c8",
+    "url": "/Robofriend/static/js/main.57b687de.chunk.js"
+  },
+  {
+    "revision": "14cab223565f1a0844b8",
+    "url": "/Robofriend/static/js/runtime-main.215f64ae.js"
+  }
+]);
